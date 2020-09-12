5c80ba96cb0e9a0b770fc5b1145f9c8d56d4778c
Stephen kac <stealekac@gmail.com>
2020-09-12 16:04:08 -0600

    Created FeedItem

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Stephen Kac on 9/6/20.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
