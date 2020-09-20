80569b5813402da63eadbdc65725c5e941565cee
Stephen <stealekac@gmail.com>
2020-09-19 19:42:23 -0600

    Delivers invalid data error on non 200 HTTP response

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -26,7 +26,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 		XCTAssertEqual(client.requestedURLs, [url])
 	}
 	
-	func test_loadtrice_requestsDataFromURLTwice() {
+	func test_loadtwice_requestsDataFromURLTwice() {
 		let url = URL(string: "https://a-given-url.com")!
 		let (sut, client) = makeSUT(url: url)
 		
@@ -48,6 +48,17 @@ class RemoteFeedLoaderTests: XCTestCase {
 		XCTAssertEqual(capturedErrors, [.connectivity])
 	}
 	
+	func test_load_deliversErrorOnNon200HTTPResponse() {
+		let (sut, client) = makeSUT()
+		
+		var capturedErrors = [RemoteFeedLoader.Error]()
+		sut.load { capturedErrors.append($0) }
+		
+		client.complete(withStatusCode: 400)
+		
+		XCTAssertEqual(capturedErrors, [.invalidData])
+	}
+	
 	//MARK: - Helpers
 	
 	private func makeSUT(url: URL = URL(string:  "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -58,18 +69,23 @@ class RemoteFeedLoaderTests: XCTestCase {
 	
 	private class HTTPClientSpy: HTTPClient {
 		
-		private var messages = [(url: URL, completion: (Error) -> Void)]()
+		private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
 		
 		var requestedURLs: [URL] {
 			messages.map(\.url)
 		}
 		
-		func get(from url: URL, completion: @escaping (Error) -> Void) {
+		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
 			messages.append((url, completion))
 		}
 		
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error)
+			messages[index].completion(error, nil)
+		}
+		
+		func complete(withStatusCode code: Int, at index: Int = 0) {
+			let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+			messages[index].completion(nil, response)
 		}
 	}
 }
